1576f316d0ba582e5898e8916346790357541f69
Markkiogora2415 <kiogora2415@gmail.com>
2025-01-08 16:54:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.getElementById("donationForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    // Simple form validation or other JS functionality can go here
     alert("Thank you for your donation!");
 });
